b7dbd1a9b0534cd2e169f6ea7086fd6c83ffca31
akrammuh <amuh1461@gmail.com>
2025-06-03 15:33:57 +0800

    update terbaruuuuu

--- assets/js/main.js
+++ assets/js/main.js
@@ -361,3 +361,15 @@ window.addEventListener('load', function () {
     preloader.style.display = 'none';
   }, 500); 
 });
+
+
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+  document.onkeydown = function(e) {
+    if (e.keyCode == 123 || 
+       (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) || 
+       (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) || 
+       (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0))) {
+        return false;
+    }
+  }
